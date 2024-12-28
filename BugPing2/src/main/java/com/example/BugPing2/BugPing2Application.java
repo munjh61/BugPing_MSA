291203179763ad581e9a2f0117047a5d4c3ddd83
junho <junho9661@gmail.com>
2024-12-28 21:00:06 +0900

    BugPing2 first

---
+++ BugPing2/src/main/java/com/example/BugPing2/BugPing2Application.java
@@ -0,0 +1,13 @@
+package com.example.BugPing2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugPing2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugPing2Application.class, args);
+	}
+
+}
---
+++ BugPing2/src/test/java/com/example/BugPing2/BugPing2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BugPing2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugPing2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
